77dfc989bf6fe76830d8a77a6dc85578a8e1fa10
sabyasachirakshit <sabyasachirakshitacs19@gmail.com>
2022-12-08 16:00:56 +0530

    updated login logout

--- user/forms.py
+++ user/forms.py
@@ -3,12 +3,16 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
 
-class UserRegistrationForm(UserCreationForm):
-    first_name = forms.CharField(max_length=101)
-    last_name = forms.CharField(max_length=101)
-    email = forms.EmailField()
+class SignupForm(UserCreationForm):
+    first_name = forms.CharField(
+        max_length=50, required=False, help_text='Optional.')
+    last_name = forms.CharField(
+        max_length=50, required=False, help_text='Optional.')
+    email = forms.EmailField(
+        max_length=120, help_text='Required. Enter a valid email address.')
+    birth_date = forms.DateField(help_text='Required. Format: YYYY-MM-DD')
 
     class Meta:
         model = User
-        fields = ['username', 'first_name', 'last_name',
-                  'email', 'password1', 'password2']
+        fields = ('username', 'first_name', 'last_name', 'email',
+                  'birth_date', 'password1', 'password2')
--- user/urls.py
+++ user/urls.py
@@ -9,4 +9,6 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('home',views.home),
     path('register/', views.register, name='register'),
+    path('login/', auth_views.LoginView.as_view(template_name='user/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='user/logout.html'), name='logout'),
 ]
